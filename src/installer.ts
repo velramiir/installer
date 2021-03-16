17c338f669a99aed0e571843b7a593efb4b5309b
Christian Friedow <christian@friedow.com>
2021-03-16 16:07:32 +0100

    Polish user interface

--- src/installer.ts
+++ src/installer.ts
@@ -1,15 +1,23 @@
-import fs from 'fs';
+import fs from "fs";
 import clear from "clear";
 import chalk from "chalk";
 import figlet from "figlet";
 import inquirer from "inquirer";
 import handlebars from "handlebars";
-import cmd from 'node-cmd';
-import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
-import pwgenerate from 'generate-password';
-import { stream } from 'winston';
+import { uniqueNamesGenerator, adjectives, colors, animals } from "unique-names-generator";
+import pwgenerate from "generate-password";
+import { exec } from "child_process";
 
 class Installer {
+  private static async exec(command: string): Promise<string> {
+    return new Promise(function (resolve, reject) {
+      exec(command, function (err, stdout) {
+        if (err !== null) reject(err);
+        else resolve(stdout);
+      });
+    });
+  }
+
   private static introduce(): void {
     clear();
 
@@ -36,209 +44,217 @@ class Installer {
     console.log();
   }
 
+  private static validateNotEmpty(input: string): boolean | string {
+    if (input) return true;
+    return "This field is required.";
+  }
+
   private static validateHostname(serverAddress: string): boolean | string {
+    const isNotEmpty = Installer.validateNotEmpty(serverAddress);
+    if (isNotEmpty !== true) return isNotEmpty;
+
     const isValid = !!serverAddress.match(
       /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\:\-]*[A-Za-z0-9])$/
     );
-    return isValid ? isValid : "Please enter a valid hostname.";
-  }
-
-  private static validateNotEmpty(input: string): boolean | string {
-    if (input) return true;
-    return 'This field is required.';
+    return isValid ? true : "Please enter a valid hostname.";
   }
 
   private static validateAbsolutePath(path: string): boolean | string {
-    const isValid = !!path.match(/^\/.*\/?$/);
-    return isValid ? isValid : "Please enter a valid absolute path.";
+    try {
+      const isNotEmpty = Installer.validateNotEmpty(path);
+      if (isNotEmpty !== true) return isNotEmpty;
+
+      const isAbsolutePath = !!path.match(/^\/.*\/?$/);
+      if (!isAbsolutePath) throw new Error();
+      
+      const fileExists = fs.lstatSync(path).isFile();
+      if (!fileExists) throw new Error();
+
+      return true;
+    } catch {
+      return "Please enter a valid absolute path.";
+    }
   }
 
   private static get configQuestions(): inquirer.QuestionCollection {
     return [
       {
         name: "HOSTNAME",
+        prefix: chalk.green("\n?"),
         message:
-          "Which hostname will dot.base run on (e.g. demo.dotbase.org)?" +
-          chalk.gray("\n  (e.g. dotbase.org // demo.dotbase.org)"),
+          "Which hostname will dot.base run on?" +
+          chalk.gray("\n! e.g. dotbase.org or demo.dotbase.org"),
+        suffix: chalk.green("\n>"),
         type: "input",
-        validate: Installer.validateNotEmpty ?? Installer.validateHostname,
+        validate: Installer.validateHostname,
       },
 
       {
-        name: "USE_HTTPS",
-        message:
-          "Do you want to use HTTPS?" +
-          chalk.gray("\n  (Requires tls certificate files for your domain.)"),
-        type: "confirm",
-        default: true,
-      },
-      {
         name: "TLS_CERT_PATH",
+        prefix: chalk.green("\n==="),
         message:
+          "HTTPS Configuration" +
+          chalk.green(" ===") +
+          chalk.green("\n\n? ") +
           "Please enter the path of the tls certificate for your domain." +
-          chalk.gray("\n  (e.g. /etc/letsencrypt/live/<HOSTNAME>/cert.pem)"),
+          chalk.gray("\n! e.g. /etc/letsencrypt/live/<HOSTNAME>/cert.pem"),
+        suffix: chalk.green("\n>"),
         type: "input",
-        when: (answers) => answers.USE_HTTPS,
-        validate: Installer.validateNotEmpty ?? Installer.validateAbsolutePath,
+        validate: Installer.validateAbsolutePath,
       },
       {
         name: "TLS_KEY_PATH",
+        prefix: chalk.green("\n?"),
         message:
           "Please enter the location of the private key for this certificate." +
-          chalk.gray("\n  (e.g. /etc/letsencrypt/live/<HOSTNAME>/key.pem)"),
+          chalk.gray("\n! e.g. /etc/letsencrypt/live/<HOSTNAME>/key.pem"),
+        suffix: chalk.green("\n>"),
         type: "input",
-        when: (answers) => answers.USE_HTTPS,
-        validate: Installer.validateNotEmpty ?? Installer.validateAbsolutePath,
+        validate: Installer.validateAbsolutePath,
       },
 
       {
         name: "USE_CUSTOM_CA",
+        prefix: chalk.green("\n?"),
         message: "Does your organization use a custom certification authority (CA)?",
+        suffix: chalk.green("\n>"),
         type: "confirm",
         default: false,
       },
       {
         name: "CUSTOM_CA_PATH",
+        prefix: chalk.green("\n?"),
         message: "Please enter the path of the file containing the custom CA chain.",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_CUSTOM_CA,
-        validate: Installer.validateNotEmpty ?? Installer.validateAbsolutePath,
+        validate: Installer.validateAbsolutePath,
       },
 
       {
         name: "USE_EXTENSIONS",
+        prefix: chalk.green("\n?"),
         message:
           "Do you want to use dot.base extensions to integrate it into your hospital?" +
-          chalk.gray("\n  (Requires an access key to the private dot.base container registry.)"),
+          chalk.gray("\n! Requires an access key to the private dot.base container registry."),
+        suffix: chalk.green("\n>"),
         type: "confirm",
         default: false,
       },
       {
         name: "EXTENSIONS_TO_USE",
+        prefix: chalk.green("\n?"),
         message: "Which extension do you want to use?",
+        suffix: chalk.green("\n>"),
         when: (answers) => answers.USE_EXTENSIONS,
         type: "checkbox",
         choices: [
           {
-            name: "Health Data Platform",
-            value: "hdpAdapter",
-          },
-          {
-            name: "Medtronic - Deep Brain Stimulation",
-            value: "medtronicDbsApi",
-          },
-          {
-            name: "Comm Server (ish. med)",
-            value: "commServerAdapter",
-          },
-          {
-            name: "LDAP Server",
+            name: "LDAP Single Sign-on",
             value: "ldapServer",
           },
         ],
       },
-      {
-        name: "GITHUB_ACCESS_TOKEN",
-        message:
-          "Please enter your personal access token to the GitHub container registry (ghcr.io)." +
-          chalk.gray("\n  (Please also verify that you have access to the dot.base organization on GitHub.)"),
-        type: "input",
-        when: (answers) => answers.USE_EXTENSIONS,
-        validate: Installer.validateNotEmpty,
-      },
 
       {
         name: "LDAP_SERVICE_USER_EMAIL",
+        prefix: chalk.green("\n==="),
         message:
-          "LDAP Credentials" +
-          chalk.gray("\n  (Please use a service user for this.)") +
-          "\n  DLAP Username:",
+          "LDAP Connection" +
+          chalk.green(" ===") +
+          chalk.gray("\n! Please use a service user for this.") +
+          chalk.green("\n\n? ") +
+          "DLAP Username:",
+        suffix: chalk.green("\n>"),
         type: "input",
-        when: (answers) => answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes('ldapServer'),
+        when: (answers) =>
+          answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes("ldapServer"),
         validate: Installer.validateNotEmpty,
       },
       {
         name: "LDAP_SERVICE_USER_PASSWORD",
+        prefix: chalk.green("\n?"),
         message: "DLAP Password:",
+        suffix: chalk.green("\n>"),
         type: "input",
-        when: (answers) => answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes('ldapServer'),
+        when: (answers) =>
+          answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes("ldapServer"),
         validate: Installer.validateNotEmpty,
       },
 
       {
         name: "USE_SENTRY",
+        prefix: chalk.green("\n?"),
         message:
           "Do you want to use Sentry to monitor the application state?" +
-          chalk.gray("\n  You will need an own sentry account for this."),
+          chalk.gray("\n! You will need an own sentry account for this."),
+        suffix: chalk.green("\n>"),
         type: "confirm",
         default: false,
       },
       {
         name: "SENTRY_ENVIRONMENT",
+        prefix: chalk.green("\n?"),
         message: "Which sentry environment do you want to use?",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_SENTRY,
         validate: Installer.validateNotEmpty,
       },
       {
         name: "MEDICAL_DASHBOARD_SENTRY_DSN",
+        prefix: chalk.green("\n==="),
         message:
-          "Please enter Sentry DSNs for the following applications to monitor them." +
-          chalk.gray("\n  Please see the sentry docs pages on how to create applications and get the DSNs.") +
-          "\n Medical Dashboard:",
+          "Sentry DSNs" +
+          chalk.green(" ===") +
+          chalk.gray(
+            "\n! Please enter Sentry DSNs for the following applications to monitor them."
+          ) +
+          chalk.gray(
+            "\n! Please see the sentry docs pages on how to create applications and get the DSNs."
+          ) +
+          chalk.green("\n\n? ") +
+          "Medical Dashboard:",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_SENTRY,
         validate: Installer.validateNotEmpty,
       },
       {
         name: "FHIR_SERVER_SENTRY_DSN",
+        prefix: chalk.green("\n?"),
         message: "FHIR Servers:",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_SENTRY,
         validate: Installer.validateNotEmpty,
       },
       {
         name: "FILE_STORAGE_API_SENTRY_DSN",
+        prefix: chalk.green("\n?"),
         message: "File Storage Service:",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_SENTRY,
         validate: Installer.validateNotEmpty,
       },
       {
         name: "ICD_10_API_SENTRY_DSN",
+        prefix: chalk.green("\n?"),
         message: "ICD 10 API:",
+        suffix: chalk.green("\n>"),
         type: "input",
         when: (answers) => answers.USE_SENTRY,
         validate: Installer.validateNotEmpty,
       },
-      {
-        name: "COMM_SERVER_ADAPTER_SENTRY_DSN",
-        message: "Comm Server Adapter:",
-        type: "input",
-        when: (answers) => answers.USE_SENTRY && answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes('commServerAdapter'),
-        validate: Installer.validateNotEmpty,
-      },
-      {
-        name: "HDP_ADAPTER_SENTRY_DSN",
-        message: "HDP Adapter:",
-        type: "input",
-        when: (answers) => answers.USE_SENTRY && answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes('hdpAdapter'),
-        validate: Installer.validateNotEmpty,
-      },
-      {
-        name: "MEDTRONIC_DBS_API_SENTRY_DSN",
-        message: "Medtronic DBS API:",
-        type: "input",
-        when: (answers) => answers.USE_SENTRY && answers.USE_EXTENSIONS && answers.EXTENSIONS_TO_USE.includes('medtronicDbsApi'),
-        validate: Installer.validateNotEmpty,
-      },
-
 
       {
         name: "CREATE_INSTANCE",
+        prefix: chalk.green("\n?"),
         message:
           "Do you want to create a dot.base instance with this configuration?" +
-          chalk.gray("\n  Please double check the information above."),
+          chalk.gray("\n! Please double check the information above."),
+        suffix: chalk.green("\n>"),
         type: "confirm",
         default: true,
       },
@@ -255,23 +271,29 @@ class Installer {
     const config = await Installer.inquireConfig();
     if (!config.CREATE_INSTANCE) return;
 
-    Installer.fillConfigTemplate('dotbase-realm.json', config);
-    Installer.createDockerSecret('dotbase-realm.json', 'deployment/dotbase-realm.json');
+    try {
+      Installer.fillConfigTemplate("dotbase-realm.json", config);
+      await Installer.createDockerSecret("dotbase-realm.json", "deployment/dotbase-realm.json");
 
-    if (config.USE_HTTPS) {
-      Installer.createDockerConfig('traefik.toml', 'src/config/traefik.toml');
-      Installer.createDockerSecret('cert.pem', config.TLS_CERT_PATH);
-      Installer.createDockerSecret('key.pem', config.TLS_KEY_PATH);
-    } else {
-      Installer.createEmptyDockerConfig('traefik.toml');
-      Installer.createEmptyDockerSecret('cert.pem');
-      Installer.createEmptyDockerSecret('key.pem');
-    }
+      
+      await Installer.createDockerConfig("traefik.toml", "config/traefik.toml");
+      await Installer.createDockerSecret("cert.pem", config.TLS_CERT_PATH);
+      await Installer.createDockerSecret("key.pem", config.TLS_KEY_PATH);
 
-    if (config.USE_CUSTOM_CA) {
-      Installer.createDockerConfig('custom-ca.crt', config.CUSTOM_CA_PATH);
-    } else {
-      Installer.createEmptyDockerConfig('custom-ca.crt');
+      if (config.USE_CUSTOM_CA) {
+        await Installer.createDockerConfig("custom-ca.crt", config.CUSTOM_CA_PATH);
+      } else {
+        await Installer.createEmptyDockerConfig("custom-ca.crt");
+      }
+    } catch (e) {
+      console.log(
+        chalk.red(
+          "Could not create dot.base instance because there are existing dot.base configuration files." +
+            "\nAt this time we do not support the installation of multiple dot.base instances on one machine." +
+            "\nPlease reach out to our support team at movebase@charite.de to setup more sophisticated systems."
+        )
+      );
+      return;
     }
 
     config.AUTH_CLIENT_SECRET = Installer.generatePassword();
@@ -286,17 +308,16 @@ class Installer {
     config.FHIR_DB_USER = Installer.generateUsername();
     config.FHIR_DB_PASSWORD = Installer.generatePassword();
 
-    Installer.fillConfigTemplate('parameters.yml', config);
+    Installer.fillConfigTemplate("parameters.yml", config);
 
-    Installer.enableExperimentalDockerFeatures();
-    Installer.launchDotBase();
+    await Installer.launchDotBase();
   }
 
   private static fillConfigTemplate(filename: string, config: inquirer.Answers) {
-    const configFileTemplate = fs.readFileSync(`src/config/templates/${filename}`).toString();
+    const configFileTemplate = fs.readFileSync(`config/templates/${filename}`).toString();
     const configFile = handlebars.compile(configFileTemplate);
-    
-    this.createDirectory('deployment');
+
+    this.createDirectory("deployment");
     fs.writeFileSync(`deployment/${filename}`, configFile(config));
   }
 
@@ -304,24 +325,16 @@ class Installer {
     if (!fs.existsSync(name)) fs.mkdirSync(name);
   }
 
-  private static createDockerSecret(name: string, file: string) {
-    cmd.runSync(`docker secret rm ${name}`);
-    cmd.runSync(`docker secret create ${name} ${file}`);
+  private static async createDockerSecret(name: string, file: string) {
+    await Installer.exec(`docker secret create ${name} ${file}`);
   }
 
-  private static createDockerConfig(name: string, file: string) {
-    cmd.runSync(`docker config rm ${name}`);
-    cmd.runSync(`docker config create ${name} ${file}`);
+  private static async createDockerConfig(name: string, file: string) {
+    await Installer.exec(`docker config create ${name} ${file}`);
   }
 
-  private static createEmptyDockerSecret(name: string) {
-    cmd.runSync(`docker secret rm ${name}`);
-    cmd.runSync(`echo "" | docker secret create ${name} -`);
-  }
-
-  private static createEmptyDockerConfig(name: string) {
-    cmd.runSync(`docker config rm ${name}`);
-    cmd.runSync(`echo "" | docker config create ${name} -`);
+  private static async createEmptyDockerConfig(name: string) {
+    await Installer.exec(`echo "" | docker config create ${name} -`);
   }
 
   private static generateUsername(): string {
@@ -331,20 +344,17 @@ class Installer {
   }
 
   private static generatePassword(): string {
-      return pwgenerate.generate({
-          length: Math.floor(Math.random() * 5) + 50,
-          strict: true,
-          numbers: true,
-      })
+    return pwgenerate.generate({
+      length: Math.floor(Math.random() * 5) + 50,
+      strict: true,
+      numbers: true,
+    });
   }
 
-  private static enableExperimentalDockerFeatures() {
-    cmd.runSync('export DOCKER_CLI_EXPERIMENTAL=enabled');
-  }
-
-  private static launchDotBase() {
-    const output = cmd.runSync(`docker app run --parameters-file deployment/parameters.yml ghcr.io/dot-base/dot-base:latest`);
-    console.log(output);
+  private static async launchDotBase() {
+    await Installer.exec(
+      `export DOCKER_CLI_EXPERIMENTAL=enabled && docker app run --parameters-file deployment/parameters.yml ghcr.io/dot-base/dot-base:latest`
+    );
   }
 
   constructor() {
